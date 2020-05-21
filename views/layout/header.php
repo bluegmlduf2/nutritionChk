5c4a63bc9e4ff038d53d610c3f71d8de48ef2645
chason7942 <chason7942@gmail.com>
2020-05-21 23:42:38 +0900

    받아랏 헤더

--- views/layout/header.php
+++ views/layout/header.php
@@ -5,10 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">    
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="../application/libraries/style/common.css">
+    <link rel="stylesheet" href="../application/libraries/plugin/datatables.min.css"><!--datatables-->
     <link rel="stylesheet" href="../application/libraries/style/nm_form.css">
     <link rel="stylesheet" href="../application/libraries/style/nm_list.css">
     <script src="../application/libraries/plugin/jquery-3.5.0.min.js"></script><!--jQuery-->
-    <link rel="stylesheet" href="../application/libraries/plugin/datatables.min.css"><!--datatables-->
     <script src="../application/libraries/plugin/datatables.min.js"></script><!--datatables-->
-
 </head>
